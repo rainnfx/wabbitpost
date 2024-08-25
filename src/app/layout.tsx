c3075df94d73164e18a3ab4f900f341560143387
rainnfx <matteo.aure@gmail.com>
2024-08-25 14:03:09 -0700

    init 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import "@/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
+import { SessionProvider } from "next-auth/react";
+
 import { TRPCReactProvider } from "@/trpc/react";
+import { auth } from "@/server/auth";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -11,14 +14,17 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const session = await auth();
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
-      </body>
-    </html>
+    <SessionProvider session={session}>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body>
+          <TRPCReactProvider>{children}</TRPCReactProvider>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
